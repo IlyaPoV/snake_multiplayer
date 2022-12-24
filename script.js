47b8e843821bcb5a50d372f03874f4a177cf674e
ilyapov <ivpolonikov@gmail.com>
2022-12-24 13:20:17 +0500

    OOP modernization

--- script.js
+++ script.js
@@ -14,59 +14,69 @@ function GameManager() {
     this.snake = new Snake();
     this.snake1 = new Snake();
 
-    this.restart = function() {
+    this.update = this.update.bind(this);
+}
+GameManager.prototype = {
+    update: function() {
+        requestAnimationFrame(this.update);
+    
+        if (++count < 4) {
+            return;
+        }
+    
+        count = 0;
+        context.clearRect(0,0, canvas.width, canvas.height);
+    
+        this.snake.moving();
+        this.snake1.moving();
+        this.apple.render();
+    },
+    restart:function() {
         this.apple.createNew();
         this.snake.init(["ArrowLeft", "ArrowRight"], ["ArrowUp", "ArrowDown"],);
         this.snake1.init(['a', 'd'], ['w', 's'], 'blue');
     }
-
-    this.restart();
 }
 
-function Cell(x,y,parentCells) {
-    this.parentCells = parentCells;
+function Cell(x,y) {
     this.x = x;
     this.y = y;
-
-    this.render = function(color="green") {
+}
+Cell.prototype = {
+    render: function(color) {
         context.fillStyle = color;
         context.fillRect(this.x, this.y, GRID - 1, GRID - 1);
-    };
-
-    this.collideApple = function() {
+    },
+    collideApple: function() {
         if(this.x == gameManager.apple.x && this.y == gameManager.apple.y) {
-            parentCells.maxCells++;
-            gameManager.apple.createNew();
-        };
-    };
-}
-
-function Cells (parentSnake) {
-    this.maxCells = 4;
-
-    this.update = function(x, y) {
-        this.unshift(new Cell(x, y, this));
-    
-        if(this.length > this.maxCells) {
-            this.pop(this.length);
+            return true;
         }
-
-        this.forEach(function (cell) {
-            cell.render(parentSnake.color);
-            cell.collideApple(this);
-        });
-
-        if(this.checkSelfByte()) {
-            parentSnake.createNew();
-        }
+        return false;
     }
+}
+    
+function Cells () {
+    this.maxCells = 4;
+};
+Cells.prototype = Object.create(Array.prototype);
+Cells.prototype._checkSelfByte = function() {
+    return this.filter(el=>el.x==this[0].x && el.y==this[0].y).length > 1;
+}
+Cells.prototype.update = function(x, y) {
+    this.unshift(new Cell(x, y));
 
-    this.checkSelfByte = function() {
-        return this.filter(el=>el.x==this[0].x && el.y==this[0].y).length > 1;
+    if(this.length > this.maxCells) {
+        this.pop(this.length);
     }
 };
-
-Cells.prototype = Array.prototype;
+Cells.prototype.render = function(color) {
+    this.forEach(function (cell) {
+        cell.render(color);
+    });
+}
+Cells.prototype.collideApple = function() {
+    return this.filter(cell=> cell.collideApple()).length > 0;
+}
 
 function Snake() {
     this.x;
@@ -75,16 +85,11 @@ function Snake() {
     this.dx;
     this.color;
     this.cells = new Cells(this);
-
-    this.moving = function() {
-        this.x += this.dx;
-        this.y += this.dy;
-        this.wallWalking();
-
-        this.cells.update(this.x, this.y);
-    }
-
-    this.wallWalking = function() {
+    
+    this.createNew = this.createNew.bind(this);
+};
+Snake.prototype = {
+    _wallWalking: function() {
         if(this.x >= canvas.width) {
             this.x = 0;
         }
@@ -100,18 +105,22 @@ function Snake() {
         if(this.y < 0) {
             this.y = canvas.height - GRID;
         }
-    };
-
-    this.createNew = function() {
+    },
+    createNew: function() {
         this.x = 160;
         this.y = 160;
         this.dy = GRID;
         this.dx = 0;
         this.cells = new Cells(this);
+        this.cells.color = this.color
         this.cells.maxCells = 4;
-    }
-
-    this.controll = function (xMoveKeys = ["ArrowLeft", "ArrowRight"], yMoveKeys = ["ArrowUp", "ArrowDown"]) {
+    },
+    init: function(xMoveKeys, yMoveKeys, color="green") {
+        this.color = color;
+        this.controll(xMoveKeys, yMoveKeys);
+        this.createNew();
+    },
+    controll: function (xMoveKeys, yMoveKeys) {
         document.addEventListener('keydown', function({key}){
             if(this.dy === 0 && yMoveKeys.includes(key)) {
                 const proxyGrid = key == yMoveKeys[0] ? -GRID : GRID;
@@ -126,54 +135,44 @@ function Snake() {
             }
         
         }.bind(this));
-    }
+    },
+    moving: function() {
+        this.x += this.dx;
+        this.y += this.dy;
+        this._wallWalking();
 
-    this.init = function(xMoveKeys, yMoveKeys, color="green") {
-        this.color = color;
-        this.controll(xMoveKeys, yMoveKeys);
-        this.createNew();
+        this.cells.update(this.x, this.y);
+        this.cells.render(this.color);
+        if(this.cells.collideApple()) {
+            this.cells.maxCells++;
+            gameManager.apple.createNew();
+        }
+
+        if(this.cells._checkSelfByte()) {
+            this.createNew();
+        }
     }
 };
 
 function Apple() {
     this.x;
     this.y;
+};
 
-    this.render = function() {
+Apple.prototype = {
+    render: function() {
         context.fillStyle = "red";
         context.fillRect(this.x, this.y, GRID-1, GRID-1);
-    }
+    },
 
-    this.createNew = function() {
+    createNew: function() {
         this.x = getRandomInt(0, 25) * GRID;;
         this.y = getRandomInt(0, 25) * GRID;
     }
-
-    this.createNew();
-};
+}
 
 const gameManager = new GameManager();
 
-// игровой цикл
-
-
-function Update() {
-    requestAnimationFrame(Update);
-
-    if (++count < 4) {
-        return;
-    }
-
-    count = 0;
-    context.clearRect(0,0, canvas.width, canvas.height);
-
-    gameManager.snake.moving();
-    gameManager.snake1.moving();
-
-    gameManager.apple.render();
-
-    };
-
-
-
-requestAnimationFrame(Update);
+gameManager.restart();
+gameManager.update();
+// requestAnimationFrame(Update);
